6deb7123926b83f2c5b7b51ad23ee5d48e99ac5f
Lukas <lukas.bca.acc@outlook.de>
2020-06-20 01:06:00 +0200

    - Added menu   * Create user   * Create database

--- sql_setup.py
+++ sql_setup.py
@@ -1,33 +1,88 @@
 ##############################
 # SQLITE fuer die Stechuhr   #
 ##############################
-
 import sqlite3
-from time import strftime
 from typing import List
+import configparser
+import random, string
 
-verbindung = sqlite3.connect("./Timedb.sqlite")
+#################
+#               #
+#    CONFIG     #
+#               #
+#################
+configparser = configparser.RawConfigParser()
+configFilePath = r'conf/app.conf'
+configparser.read(configFilePath)
+
+def randomword(length):
+   letters = string.ascii_lowercase
+   return ''.join(random.choice(letters) for i in range(length))
+
+
+db = configparser.get('ServerSettings', 'db')
+
+verbindung = sqlite3.connect("./testdb.sqlite")
 cursor = verbindung.cursor()
+befehle: List[str] = []
 
 ############ SQL-Section ############
 
-befehle: List[str] = []
+print(
+    "SQL-Tool zur Zeiterfassung"
+    "\n"
+    "Vorsicht bei der Anwendung!"
+)
 
-#Tabelle Anwesenheit erstellen (Array id:0)
-befehle.append('''CREATE TABLE IF NOT EXISTS Anwesenheit (
-	id_anwesenheit INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	id_mitarbeiter CHAR NOT NULL,
-	time_kommen TEXT NOT NULL,
-	time_gehen TEXT)
-	''')
+print(
+    "\n"
+    "Bitte Auswahl treffen \n"
+    "1 - Neuen Benutzer  \n"
+    "2 - Datenbank aufsetzen \n"
+    "q - Beenden \n"
+)
+while True:
+    eingabe = input("Auswahl eingeben: ")
+    print(eingabe)
+    if (eingabe == "1"):
+        print("Neuen Benutzer anlegen")
+        id_mitarbeiter = "NULL"
+        name = input("Name eingeben: ")
+        nachname = input("Nachname eingaben: ")
+        raum = input("Raum eingeben: ")
+        raum = str(raum)
+        position = input("Position eingaben: ")
+        position = str(position)
 
-#Tabelle Status erstellen (Array id:1)
-befehle.append('''CREATE TABLE IF NOT EXISTS Status (
-	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	id_mitarbeiter CHAR NOT NULL,
-	status BOOL NOT NULL)
-	''')
+        random_id = randomword(5)
+        cursor.execute('INSERT INTO mitarbeiter VALUES ('"?"', '"?"', '"?"', '"?"', '"?"', 0)', (random_id, name, nachname,
+                                                                                   raum, position))
+        verbindung.commit()
+    elif (eingabe == "2"):
+        #Tabelle Anwesenheit erstellen (Array id:0)
+        befehle.append('''CREATE TABLE IF NOT EXISTS anwesenheit (
+        	id_anwesenheit INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        	id_mitarbeiter CHAR NOT NULL,
+        	datum CHAR NOT NULL,
+        	time_in TEXT NOT NULL,
+        	time_out TEXT)
+        	''')
 
-cursor.execute(befehle[0])
-cursor.execute(befehle[1])
-verbindung.commit()
+        #Tabelle Status erstellen (Array id:1)
+        befehle.append('''CREATE TABLE IF NOT EXISTS mitarbeiter (
+        	id_mitarbeiter TEXT NOT NULL PRIMARY KEY,
+        	name TEXT NOT NULL,
+        	nachname TEXT NOT NULL,
+        	raum TEXT,
+        	position TEXT,
+        	status BOOL NOT NULL)
+        	''')
+
+        cursor.execute(befehle[0])
+        cursor.execute(befehle[1])
+        verbindung.commit()
+    elif (eingabe == "q"):
+        cursor.close()
+        break
+    else:
+        print("Fehler bei der Eingabe. Bitte erneut versuchen")
